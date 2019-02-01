8709095c05f306b0a68a8f5d85a24526a292d02c
ividito <ividito@gmail.com>
2019-02-01 11:15:29 -0400

    Package placeholders

---
+++ CrawlerInterface/__init__.py
@@ -0,0 +1,3 @@
+# The crawler interface can work with a user-defined crawler (which we will implement)
+# It can also be used to pass various arguments (URLS, Regex, etc) to a 3rd party crawler
+# It also returns the output of the crawler, depending on format, to the system to be loaded to a DB
---
+++ DataExportInterface/__init__.py
@@ -0,0 +1,1 @@
+# Package for exporting crawler data to a database or local Excel file
