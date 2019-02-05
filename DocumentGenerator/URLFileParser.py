8fd380f2fe9a85a2012b65a35c2cd16576109450
ividito <ividito@gmail.com>
2019-02-05 09:02:01 -0400

    URL parser added

--- DocumentGenerator/URLFileParser.py
+++ DocumentGenerator/URLFileParser.py
@@ -1,8 +1,29 @@
+from DocumentGenerator import *
 
 
 class URLFileParser:
 
-    def __init__(self):
-        pass
+    def __init__(self, fin):
+        if fin:
+            self.filename = fin
+        else:
+            raise AttributeError(message="Must provide filename to URLFileParser")
+        self.urlParser = URLExtractor()
+        self.fileEntity = open(self.filename, 'r')  # create readable file object
 
-    # Given a filename for a file containing 1 URL per line, get a list of URL strings and their document type
+    def read_single_url(self, urlline):
+        protocol = urlline.split(sep=":")[0]
+        url = ''.join(urlline.split(sep=":")[1:])
+        parsedUrl = self.urlParser.parse(url, protocol)
+        return parsedUrl
+
+    def read_all_urls(self):
+        urllist = []
+        for line in self.fileEntity.readlines():
+            urllist.append(self.read_single_url(line))
+        return urllist
+
+    def destroy(self):
+        # close file reader
+        self.fileEntity.close()
+        # TODO deal with created extractor objects as needed
