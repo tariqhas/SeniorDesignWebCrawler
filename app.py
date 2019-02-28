968ae7b2d52d0d619a2fc6820c656917da6963e7
ividito <ividito@gmail.com>
2019-02-28 11:33:36 -0400

    Some front-end templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -11,3 +11,34 @@ def hello_world():
 @app.route('/localcrawltest/')
 def localcrawltest():
     return 'placeholder'
+
+@app.route('/index/')
+def index():
+    example = { 1: {'name':'Mock Button'}, 2: {'name':'Mock Button'}, 3: {'name':'Mock Button'}}
+    return render_template('index.html', example = example)\
+
+@app.route('/executor/')
+def executor():
+    navi = {
+                        1:{
+                         'name': 'Example Value 1',
+                         'value': 1,
+                         },
+                        2:{
+                        'name': 'Example Value 2',
+                        'value': 2,
+                         },
+                        3:{
+                        'name': 'Example Value 3',
+                        'value': 3,
+                         },
+                        4:{
+                        'name': 'Example Value 4',
+                        'value': 4,
+                         },
+                        5:{
+                        'name': 'Example Value 5',
+                        'value': 5,
+                         }
+                    }
+    return render_template('executor.html', navigation = navi)
