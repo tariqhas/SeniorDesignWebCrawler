148a69d960a072cb3cfb7af8a81387caa02e5a74
ividito <ividito@gmail.com>
2019-03-07 16:38:50 -0400

    Added URL/Attribute Index tables (test script not updated yet)

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,16 +1,37 @@
 CREATE DATABASE IF NOT EXISTS seniordesign;
 USE seniordesign;
 
+DROP TABLE IF EXISTS UrlIndex;
+create table UrlIndex
+(
+	UrlId int auto_increment,
+	FileName varchar(255),
+	URLListName varchar(255),
+	PRIMARY KEY(UrlId)
+);
+
 DROP TABLE IF EXISTS Urls;
 create table Urls
 (
-	UrlId int auto_increment,
+	UrlId int,
 	Url varchar(255),
 	UrlType varchar(255),
 	UrlComment varchar(255),
-	Primary Key(UrlId)
+	PRIMARY KEY(UrlId, Url),
+	CONSTRAINT `fk_url_index`
+		FOREIGN KEY (UrlId) REFERENCES UrlIndex (UrlId)
+		ON DELETE CASCADE
+		ON UPDATE RESTRICT
 );
 
+DROP TABLE IF EXISTS AttributeIndex;
+create table AttributeIndex
+(
+	AttributeId int auto_increment,
+	FileName varchar(255),
+	AttributeListName varchar(255),
+	PRIMARY KEY(UrlId)
+);
 
 DROP TABLE IF EXISTS Attributes;
 create table Attributes
@@ -19,7 +40,11 @@ create table Attributes
 	AttributeName varchar(255),
 	AttributeType varchar(10),
 	Pattern varchar(255),
-	Primary Key(AttributeId)
+	PRIMARY KEY(AttributeId, AttributeName),
+	CONSTRAINT `fk_attribute_index`
+		FOREIGN KEY (AttributeId) REFERENCES AttributeIndex (AttributeId)
+		ON DELETE CASCADE
+		ON UPDATE RESTRICT
 );
 /*select * from Urls where UrlId = 1;
 Delete from Urls where UrlId = 1;
